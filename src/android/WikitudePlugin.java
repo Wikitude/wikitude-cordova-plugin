47ee43b15551a1f8a24a1f5e7f2c7f8c44c437ea
Andreas Hauser <andreas.hauser@wikitude.com>
2013-07-26 11:56:30 +0200

    updates to 3.0.0

--- src/android/WikitudePlugin.java
+++ src/android/WikitudePlugin.java
@@ -4,9 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
-import org.apache.cordova.api.CallbackContext;
-import org.apache.cordova.api.CordovaPlugin;
-import org.apache.cordova.api.PluginResult;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
