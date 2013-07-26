874bd495c9cd87c2cea1fd771be1d3e229e51d62
Andreas Hauser <andreas.hauser@wikitude.com>
2013-07-26 14:56:36 +0200

    provides one single SampleApplicaiton instead of two

---
+++ samples/Android/WikitudePluginSample/assets/www/world/1_ImageRecognition_1_ImageOnTarget/js/imagerecognition.js
@@ -0,0 +1,32 @@
+var World = {
+	loaded: false,
+
+	init: function initFn() {
+		this.createOverlays();
+	},
+
+	createOverlays: function createOverlaysFn() {
+		// Initialize Tracker
+		this.tracker = new AR.Tracker("assets/magazine.wtc", {
+			onLoaded: this.worldLoaded
+		});
+
+		// Create overlay for page one
+		var imgOne = new AR.ImageResource("assets/imageOne.png");
+		var overlayOne = new AR.ImageDrawable(imgOne, 1, {
+			offsetX: -0.15,
+			offsetY: 0
+		});
+		var pageOne = new AR.Trackable2DObject(this.tracker, "pageOne", {
+			drawables: {
+				cam: overlayOne
+			}
+		});
+	},
+
+	worldLoaded: function worldLoadedFn() {
+		document.body.removeChild(document.getElementById('loadingMessage'));
+	}
+};
+
+World.init();
---
+++ samples/Android/WikitudePluginSample/gen/com/wikitude/phonegapsamples/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wikitude.phonegapsamples;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
