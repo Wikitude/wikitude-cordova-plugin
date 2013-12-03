660a6421dd7665d2b743437745e8c5b482371b0f
AndreasSchacherbauer <a.schacherbauer@me.com>
2013-12-03 16:35:22 +0100

    Updates iOS Wikitude SDK to version 3.2.1;
    Signed-off-by: AndreasSchacherbauer <a.schacherbauer@me.com>

--- src/ios/WTArchitectView.h
+++ src/ios/WTArchitectView.h
@@ -20,29 +20,70 @@ extern NSString * const kWTScreenshotSaveModeKey;
 extern NSString * const kWTScreenshotCaptureModeKey;
 extern NSString * const kWTScreenshotImageKey;
 
-
+/**
+ *
+ * The WTARMode is used to specify which type of augmented reality is used in an ARchitect World. Based on the ARMode, different device capabilities are required and the -isDeviceSupported: method returns different results.
+ *
+ * When only image recognition Worlds are loaded, more devices are supported because e.g. the iPod touch 5th gen is missing an compass and can therefore not support geo augmented reality.
+ *
+ */
 typedef NS_ENUM(NSUInteger, WTARMode){
+    /** The ARchitect World is using both, geo augmented reality and image recognition */
     WTARMode_Geo,
+    
+    /** The ARchitect World is only using image recongnition. */
     WTARMode_IR
 };
 
+/**
+ *
+ * The WTScreenshotCaptureMode defines if the screenshot should also include the web view (The Html hud) or only the cam content.
+ *
+ */
 typedef NS_ENUM(NSUInteger, WTScreenshotCaptureMode){
-
+    
+    /** Only the camera content with all augmentations will be captured */
     WTScreenshotCaptureMode_Cam,
+    
+    /** Also the web view is composed into the final screenshot */
     WTScreenshotCaptureMode_CamAndWebView
 };
 
-typedef NS_ENUM(NSUInteger, WTScreenshotSaveMode){
 
+/**
+ *
+ * The WTScreenshotSaveMode can be used to defines the output location of the generated screenshot.
+ *
+ */
+typedef NS_ENUM(NSUInteger, WTScreenshotSaveMode){
+    
+    /** The generated screenshot will be added to the Camera Roll in the Photos application */
     WTScreenshotSaveMode_PhotoLibrary       = 1,
+    
+    /** The generated screenshot will be saved in the application bundle. File name and path can be passed into the -captureScreenWithMode:usingSaveMode:saveOptions:context: method by the context dictionary. The key for the NSString object is kWTScreenshotBundleDirectoryKey. */
     WTScreenshotSaveMode_BundleDirectory    = 2,
+    
+    /** The generated screenshot will be passed to the -architectView:didCaptureScreenWithContext: context dictionary. The key is  kWTScreenshotImageKey. */
     WTScreenshotSaveMode_Delegate           = 3
 };
 
-typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
 
+/**
+ *
+ * WTScreenshotSaveOption are used to define specific instructions that should be considered before or after the screenshot saving.
+ *
+ * The values can be chained together using the | operator.
+ *
+ */
+typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
+    
+    /** Independent of the save mode, the delegate method -architectView:didCaptureScreenWithContext: will be called. The context dictionary contains all informations regarding capture and save mode. */
     WTScreenshotSaveOption_CallDelegateOnSuccess        = 1 << 0,
+    
+    /** This option is only considered when using the save mode WTScreenshotSaveMode_BundleDirectory. It defines if a existing screenshot with the same name should be replaced or not. When a file with the same name already exists and the overwriting option is not set, the screen capture will fail and the delegate method -architectView:didFailCaptureScreenWithError will be invoked. */
     WTScreenshotSaveOption_SavingWithoutOverwriting     = 1 << 1,
+    
+    /** No options should be considered during the screenshot saving. */
     WTScreenshotSaveOption_None                         = 0
 };
 
@@ -65,7 +106,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  * @param architectView The WTArchitectView object which invoked the url.
  *
  * @param url The url which was called in javascript.
-*/ 
+ */
 - (void)architectView:(WTArchitectView *)architectView invokedURL:(NSURL *)url;
 
 
@@ -75,7 +116,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  * @param architectView The ArchitectView which failed to load the url.
  *
  * @param error An NSError object containing more informations why the load did fail.
-*/
+ */
 - (void)architectView:(WTArchitectView *)architectView didFailLoadWithError:(NSError *)error;
 
 
@@ -84,10 +125,10 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  *
  * @param architectView The ArchitectView which generated the screenshot.
  *
- * @param context A NSDictionary, containig at least CaptureMode (Key = WTScreenshotCaptureMode) and SavingMode (Key = WTScreenshotSaveMode). Optional objects are given for the different SaveModes. 
+ * @param context A NSDictionary, containig at least CaptureMode (Key = WTScreenshotCaptureMode) and SavingMode (Key = WTScreenshotSaveMode). Optional objects are given for the different SaveModes.
  *
  * @discussion The context contains different objects depending on the SaveMode. SaveMode PhotoLibrary contains the two standard objects (CaptureMode & SaveMode). SaveMode PhotoLibrary contains additional a UIImage object (the Screenshot, Key = WTScreenshotKey). SaveMode BundleDirectory contains a NSString object representing the file path in the bundle (Key = WTScreenshotPath).
-*/
+ */
 - (void)architectView:(WTArchitectView *)architectView didCaptureScreenWithContext:(NSDictionary *)context;
 
 /**
@@ -107,7 +148,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  * WTArchitectView class
  *
  *This class allows you to load and display the content from ARchitect Worlds.
- *It is the "Entry Point" to the SDK which exposes all SDK functionality. Instantiating and adding this Component to the UI should be sufficient to use ARchitect in any application. 
+ *It is the "Entry Point" to the SDK which exposes all SDK functionality. Instantiating and adding this Component to the UI should be sufficient to use ARchitect in any application.
  *
  */
 @interface WTArchitectView : UIView
@@ -116,7 +157,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
 /**
  * The object that acts as the delegate of the receiving WTArchitectView
  *
-*/ 
+ */
 @property (nonatomic, weak) id<WTArchitectViewDelegate>                                 delegate;
 @property (nonatomic, readonly) BOOL                                                    isRunning;
 @property (nonatomic, assign) CLLocationAccuracy                                        desiredLocationAccuracy;
@@ -147,7 +188,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  *
  * @param key Your developer key, provided with your licence information.
  * @param motionManager The CMMotionManager instance which should be used from the SDK.
-*/
+ */
 - (void)initializeWithKey:(NSString*)key motionManager:(CMMotionManager*)motionManager;
 
 
@@ -157,7 +198,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  *  @param architectWorldUrl The url that points to the Architect world. This can be eather a local .html file or a url, pointing to a file on a server or to your dropbox.
  *
  * @discussion
-*/
+ */
 - (void)loadArchitectWorldFromUrl:(NSURL *)architectWorldUrl;
 
 
@@ -165,41 +206,41 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
 /**
  * Executes the passed JavaScript string in the context of the currently loaded ARchitect World. This can be used to pass data to the ARchitect World or to notify it of external events.
  * @param javaScript A string, representing the javascript code which should be executed.
-*/ 
+ */
 - (void)callJavaScript:(NSString*)javaScript;
 
 
 /** @name Injecting Locations */
 /**
- * Injects the supplied location information. To use the injected location setUseInjectedLocation(true) has to be called. 
+ * Injects the supplied location information. To use the injected location setUseInjectedLocation(true) has to be called.
  * @param latitude The latitude that has to be simulated.
  * @param longitude The longitude that has to be simulated.
  * @param altitude The to altitude that has to be simulated.
  * @param accuracy The accuracy of the simulated location.
-*/ 
-- (void)injectLocationWithLatitude:(float)latitude longitude:(float)longitude altitude:(float)altitude accuracy:(float)accuracy;
+ */
+- (void)injectLocationWithLatitude:(CLLocationDegrees)latitude longitude:(CLLocationDegrees)longitude altitude:(CLLocationDistance)altitude accuracy:(CLLocationAccuracy)accuracy;
 
 
 /**
-* Injects the supplied location information. To use the injected location setUseInjectedLocation(true) has to be called. 
+ * Injects the supplied location information. To use the injected location setUseInjectedLocation(true) has to be called.
  * @param latitude The latitude that has to be simulated.
  * @param longitude The longitude that has to be simulated.
  * @param accuracy The accuracy of the simulated location.
-*/ 
-- (void)injectLocationWithLatitude:(float)latitude longitude:(float)longitude accuracy:(float)accuracy;
+ */
+- (void)injectLocationWithLatitude:(CLLocationDegrees)latitude longitude:(CLLocationDegrees)longitude accuracy:(CLLocationAccuracy)accuracy;
 
 
 /**
  * If true is supplied the injected location will be used. If false is supplied the default location provider will be used.
  * @param useInjectedLocation The location simulation status
-*/ 
+ */
 - (void)setUseInjectedLocation:(BOOL)useInjectedLocation;
 
 
 /**
  * True if an injected location is currently used. false if default location provider is used.
  * @return Indicates, if the location provider is simulating the injected location.
-*/ 
+ */
 - (BOOL)isUsingInjectedLocation;
 
 
@@ -207,14 +248,14 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
 /**
  * Sets the culling distance in meters. Objects in AR that are further away won’t be visible. The default value is 50000 meters (= 50 km).
  * @param cullingDistance The culling distance that has to be applied to your AR objects.
-*/
+ */
 - (void)setCullingDistance:(float)cullingDistance;
 
 
 /**
  * Retrieves the current culling distance in meters.
  * @return The current culling distance, used by the SDK.
-*/
+ */
 - (float)cullingDistance;
 
 
@@ -223,7 +264,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  * Use this method to get the current ARchitect version number
  *
  * @return The current available ARchitect verison within the SDK.
-*/
+ */
 - (NSString *)versionNumber;
 
 
@@ -231,8 +272,8 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  * Use this method to clear all cached data and requests.
  *
  * This method clears the internal SDK cache, as well as the cache used by the webView.
- * 
-*/
+ *
+ */
 - (void)clearCache;
 
 
@@ -244,7 +285,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  *
  * @param shouldAutoRotate Should your SDK view change orientation automatically
  * @param interfaceOrientation The interface orientation the device is going to take on
-*/
+ */
 - (void)setShouldRotate:(BOOL)shouldAutoRotate toInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation;
 
 
@@ -252,20 +293,20 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  * Retrieves the current auto rotate behavior.
  *
  * @return The current auto rotation option, used by the SDK.
-*/
+ */
 - (BOOL)isRotatingToInterfaceOrientation;
 
 
 /** @name Managing the WTArchitectView updates */
 /**
  * Stops all activity of the ARchitect view (suspends UI updates of background camera, AR objects etc).
-*/
+ */
 - (void)stop;
 
 
 /**
  * Starts activity of the ARchitect view (starts UI updates of background camera, AR objects etc).
-*/ 
+ */
 - (void)start;
 
 
@@ -273,7 +314,7 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
 /**
  * Returns the motion manager used by the Wikitude SDK.
  * @return  The CMMotionManager instance, provided in the method "InitializeWithKey:motionManager", otherwise nil;
-*/ 
+ */
 - (CMMotionManager*)motionManager;
 
 
@@ -283,9 +324,9 @@ typedef NS_OPTIONS(NSUInteger, WTScreenshotSaveOptions){
  *
  * @param captureMode Defines if the WebView is included in the screenshot or not.
  * @param saveMode Defines how the captured screenshot should be processed. Possible ways are to save it to the Photo Library, to a specific bundle directory or to receive a delegate call where the image is passed in.
- * @param options Defines more option for a specific SaveMode. 
+ * @param options Defines more option for a specific SaveMode.
  * @param context A NSDictionary containing information about the CaptureMode, SaveMode and SaveMode specific objects. See Protocol reference for more information.
-*/
+ */
 - (void)captureScreenWithMode:(WTScreenshotCaptureMode)captureMode usingSaveMode:(WTScreenshotSaveMode)saveMode saveOptions:(WTScreenshotSaveOptions)options context:(NSDictionary *)context;
 
 
